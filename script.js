ca0b226626a09dc71153d8778e59863e7c30c10c
unknown <hiroishi.r@triosys.co.jp>
2026-07-27 09:24:05 +0900

    初回リリースv1.0.0

---
+++ script.js
@@ -0,0 +1,4 @@
+// 「詳しく見る」ボタンをクリックしたときの動作
+document.getElementById('detail-btn').addEventListener('click', function() {
+    alert('本日のおすすめ：チーズケーキ\n\n濃厚なクリームチーズを使った自家製ケーキです。\nドリンクとのセットは650円でご提供しています。');
+});
